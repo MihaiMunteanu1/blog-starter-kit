6c80cd3d739b1904c446fd0b5ee21be71b1335e4
Mihai Munteanu <163286946+MihaiMunteanu1@users.noreply.github.com>
2025-07-05 17:01:20 +0300

    Create index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+// src/pages/index.tsx
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: 'sans-serif' }}>
+      <h1>Bun venit!</h1>
+      <p>Aceasta este prima ta pagină personalizată Next.js afișată cu Vercel.</p>
+    </main>
+  );
+}
